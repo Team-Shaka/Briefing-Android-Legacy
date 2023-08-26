7e95a0beaec51458989fb2a0aeed1d79f1d44608
kimwest00 <minseohufs@hufs.ac.kr>
2023-08-26 13:52:03 +0900

    feat:#8 add interface.model

---
+++ app/src/main/java/com/dev/briefing/data/api/BriefingApi.kt
@@ -0,0 +1,20 @@
+package com.dev.briefing.data.api
+
+import com.dev.briefing.data.model.BriefingDetailResponse
+import com.dev.briefing.data.model.BriefingResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface BriefingApi {
+    @GET("/briefings")
+    fun getBriefingKeyword(
+        @Query("briefings")brefingsDate:String,
+        @Query("type")type:String,
+    ): BriefingResponse
+
+    @GET("/briefings/{id}")
+    fun getBriefingDetail(
+        @Path("id")id:Int,
+    ): BriefingDetailResponse
+}
