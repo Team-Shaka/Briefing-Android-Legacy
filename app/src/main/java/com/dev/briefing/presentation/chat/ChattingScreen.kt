5a02dfb6d5342c55b0f40816f25ba9dad73d23f2
kimwest00 <minseohufs@hufs.ac.kr>
2023-08-27 01:30:44 +0900

    feat:#10 add chatting webview + bottomNavi 임시 수정

--- app/src/main/java/com/dev/briefing/presentation/chat/ChattingScreen.kt
+++ app/src/main/java/com/dev/briefing/presentation/chat/ChattingScreen.kt
@@ -1,16 +1,49 @@
 package com.dev.briefing.presentation.chat
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dev.briefing.BuildConfig
+import com.dev.briefing.presentation.setting.CommonHeader
+import com.google.accompanist.web.AccompanistWebChromeClient
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 
 @Composable
-fun ChattingScreen (
+fun ChattingScreen(
     modifier: Modifier = Modifier
-){
-    Text(
-        text = "Updated: 23.0ho8.07 5AM",
-        style = MaterialTheme.typography.labelMedium
-    )
+) {
+    val webViewState =
+        rememberWebViewState(
+            url = BuildConfig.WEB_URL + "storage",
+            additionalHttpHeaders = emptyMap()
+        )
+    val webViewClient = AccompanistWebViewClient()
+    val webChromeClient = AccompanistWebChromeClient()
+
+    Column {
+        CommonHeader(
+            onBackClick = {},
+          header = "채팅 기록"
+        )
+        WebView(
+            modifier = Modifier.fillMaxHeight(),
+            state = webViewState,
+            client = webViewClient,
+            chromeClient = webChromeClient,
+            onCreated = { webView ->
+                with(webView) {
+                    settings.run {
+                        javaScriptEnabled = true
+                        domStorageEnabled = true
+                        javaScriptCanOpenWindowsAutomatically = false
+                    }
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(100.dp))
+    }
 }
