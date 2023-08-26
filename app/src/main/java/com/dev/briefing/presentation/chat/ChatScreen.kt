5d35d77f07af84ccd5518995cedea5c6977d833e
kimwest00 <minseohufs@hufs.ac.kr>
2023-08-27 00:18:41 +0900

    feat:#10 add webview

--- app/src/main/java/com/dev/briefing/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/dev/briefing/presentation/chat/ChatScreen.kt
@@ -1,16 +1,40 @@
 package com.dev.briefing.presentation.chat
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.dev.briefing.BuildConfig
+import com.google.accompanist.web.AccompanistWebChromeClient
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 
 @Composable
-fun ChatScreen (
+fun ChatScreen(
     modifier: Modifier = Modifier
-){
-    Text(
-        text = "Updated: 23.0ho8.07 5AM",
-        style = MaterialTheme.typography.labelMedium
+) {
+    val webViewState =
+        rememberWebViewState(
+            url = BuildConfig.WEB_URL + "briefChat",
+            additionalHttpHeaders = emptyMap()
+        )
+    val webViewClient = AccompanistWebViewClient()
+    val webChromeClient = AccompanistWebChromeClient()
+//    Text(
+//        text = "Updated: 23.0ho8.07 5AM",
+//        style = MaterialTheme.typography.labelMedium
+//    )
+    WebView(
+        state = webViewState,
+        client = webViewClient,
+        chromeClient = webChromeClient,
+        onCreated = { webView ->
+            with(webView) {
+                settings.run {
+                    javaScriptEnabled = true
+                    domStorageEnabled = true
+                    javaScriptCanOpenWindowsAutomatically = false
+                }
+            }
+        }
     )
 }
