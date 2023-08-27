ea6501651259589d9e7597975274ade344ccfd43
kimwest00 <minseohufs@hufs.ac.kr>
2023-08-27 17:24:31 +0900

    feat:#7 scrapScreen 연동

--- app/src/main/java/com/dev/briefing/presentation/scrap/ScrapScreen.kt
+++ app/src/main/java/com/dev/briefing/presentation/scrap/ScrapScreen.kt
@@ -17,8 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dev.briefing.data.NewsDetail
 import com.dev.briefing.presentation.setting.CommonHeader
 import com.dev.briefing.presentation.theme.MainPrimary3
@@ -32,10 +34,10 @@ fun ScrapScreen(
     modifier: Modifier = Modifier,
     onBackClick: () -> Unit,
 ) {
-    var dateList: List<LocalDate> = listOf(
-        LocalDate.of(2023, 8, 22),
-        LocalDate.of(2023, 8, 25),
-    )
+    val context = LocalContext.current
+    val viewModel: ScrapViewModel = ScrapViewModel()
+    val newsList = viewModel.getScrapData(context)
+    val dateList: List<String> = newsList.keys.toList()
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -52,8 +54,12 @@ fun ScrapScreen(
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(35.dp)
         ) {
-            items(dateList) { it ->
-                ArticleSection(localDate = it)
+//            items(dateList) { it ->
+//                ArticleSection(localDate = it,)
+//            }
+            items(newsList.entries.toList()) { entry ->
+                // entry는 Map.Entry<String, List<Int>> 타입입니다.
+                ArticleSection(localDate= entry.key, tmpNewsList= entry.value)
             }
         }
     }
@@ -63,12 +69,10 @@ fun ScrapScreen(
 @Composable
 fun ArticleSection(
     modifier: Modifier = Modifier,
-    localDate: LocalDate = LocalDate.of(2023, 8, 22),
+    localDate: String,
+    tmpNewsList:List<NewsDetail>
 ) {
-    var newsList: List<NewsDetail> = listOf(
-        NewsDetail(1, 1, "잼버리", LocalDate.of(2023, 8, 22), "fdsfd"),
-        NewsDetail(1, 2, "잼버리", LocalDate.of(2023, 8, 22), "fdsfd"),
-    )
+    val newsList= tmpNewsList.sortedBy { it.rank }
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -76,7 +80,7 @@ fun ArticleSection(
     ) {
         Text(
             modifier = Modifier.padding(start = 10.dp),
-            text = LocalDate.of(2023, 8, 22).format(DateTimeFormatter.ofPattern("yy.MM.dd")),
+            text = localDate,
             style = MaterialTheme.typography.headlineLarge
         )
         Spacer(modifier = Modifier.height(6.dp))
@@ -116,7 +120,7 @@ fun ArticleHeader(
             Text(text = news.subtitle, style = MaterialTheme.typography.labelSmall)
         }
         Text(
-            text = "23.08.07 #" + news.rank, style = MaterialTheme.typography.bodyMedium.copy(
+            text = "${news.subtitle} #" + news.rank, style = MaterialTheme.typography.bodyMedium.copy(
                 color = MainPrimary3,
                 lineHeight = 15.sp
             )
---
+++ app/src/main/java/com/dev/briefing/presentation/scrap/ScrapViewModel.kt
@@ -0,0 +1,32 @@
+package com.dev.briefing.presentation.scrap
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.dev.briefing.data.NewsDetail
+import com.dev.briefing.util.SharedPreferenceHelper
+import java.time.format.DateTimeFormatter
+
+class ScrapViewModel : ViewModel() {
+    //1.  List<date=String>를 반환
+    //2.  String - List<NewsDetail>를 반환
+    fun getScrapData(context: Context):MutableMap<String, List<NewsDetail>> {
+        var newsList: MutableMap<String, List<NewsDetail>> = mutableMapOf()
+        val scrapDateIdMap = SharedPreferenceHelper.loadDateIdMap(context)
+        val scrapIdDetailMap = SharedPreferenceHelper.loadIdDetailMap(context)
+
+
+
+        for ((key, value) in scrapDateIdMap) {
+            var tmpNewsList:MutableList<NewsDetail> = mutableListOf()
+            for (id in value){
+                var tmpDetail = scrapIdDetailMap[id]
+                tmpNewsList.add( NewsDetail(
+                    id = id, rank = tmpDetail?.rank ?: 0, title = tmpDetail?.title ?: "",
+                    date = key.format(DateTimeFormatter.ofPattern("yy.MM.dd"))?:"", subtitle = tmpDetail?.subtitle ?: ""))
+            }
+            newsList[key] = tmpNewsList
+        }
+        return newsList
+
+    }
+}
