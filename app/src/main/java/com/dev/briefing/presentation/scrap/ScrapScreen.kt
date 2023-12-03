66e70a8002c681cf29b864f010ee741ad5ded587
kimwest00 <minseohufs@hufs.ac.kr>
2023-12-03 11:43:18 +0900

    feat:#50 2.0 ui item 변경

--- app/src/main/java/com/dev/briefing/presentation/scrap/ScrapScreen.kt
+++ app/src/main/java/com/dev/briefing/presentation/scrap/ScrapScreen.kt
@@ -1,8 +1,10 @@
 package com.dev.briefing.presentation.scrap
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,16 +17,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.*
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.dev.briefing.navigation.HomeScreen
 import com.dev.briefing.presentation.theme.component.CommonHeader
 import com.dev.briefing.R
+import com.dev.briefing.data.News
 import com.dev.briefing.data.model.ScrapResponse
 import com.dev.briefing.presentation.theme.BriefingTheme
 import org.koin.androidx.compose.getViewModel
+
+@Preview
+@Composable
+fun PreviewScrapScreen() {
+    ScrapScreen(
+        onBackClick = {},
+        navController = rememberNavController()
+    )
+}
 
 @Composable
 fun ScrapScreen(
@@ -35,38 +47,37 @@ fun ScrapScreen(
     val context = LocalContext.current
     val viewModel: ScrapViewModel = getViewModel<ScrapViewModel>()
     val scrapMap = viewModel.scrapMap.observeAsState()
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background(color = BriefingTheme.color.BackgrundGray)
-            .padding(horizontal = 30.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        CommonHeader(header = "스크랩북", onBackClick = onBackClick)
+    Column {
+        CommonHeader(header = "북마크한 브리핑", onBackClick = onBackClick)
 
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(color = BriefingTheme.color.BackgroundWhite),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
 
-        if (scrapMap.value.isNullOrEmpty()) {
-            ScrapDefaultScreen()
-        } else {
+//            if (scrapMap.value.isNullOrEmpty()) {
+//                ScrapDefaultScreen()
+//            } else {
             LazyColumn(
                 modifier = Modifier.padding(vertical = 20.dp),
                 verticalArrangement = Arrangement.spacedBy(35.dp)
             ) {
-                items(scrapMap.value?.keys?.toList() ?: listOf()) { date ->
-                    scrapMap.value?.get(date)?.let {
-                        ArticleSection(
-                            localDate = date,
-                            tmpNewsList = it,
-                            navController = navController
-                        )
-                    }
+                items(2) {
+                    ScrapItem(news = ScrapResponse(
+                        briefingId = 1,
+                        title = "title",
+                        subtitle = "subtitle",
+                        ranks = 1,
+                        date = "2023-1-1"
+                    ), onItemClick = {})
                 }
 
             }
+//            }
         }
-
-
     }
 
 }
@@ -74,6 +85,7 @@ fun ScrapScreen(
 /**
  * 스크랩이 없을 때 보여줄 화면
  */
+@Preview
 @Composable
 fun ScrapDefaultScreen() {
     Column(
@@ -129,7 +141,7 @@ fun ArticleSection(
 
         ) {
             newsList.forEach { news ->
-                ArticleHeader(news = news, onItemClick = { id ->
+                ScrapItem(news = news, onItemClick = { id ->
                     navController.navigate("${HomeScreen.Detail.route}/$id")
                     Log.d("2", id.toString())
                 })
@@ -138,42 +150,57 @@ fun ArticleSection(
     }
 }
 
+@Preview
+@Composable
+fun PreviewScrapItem() {
+    ScrapItem(
+        news = ScrapResponse(
+            briefingId = 1,
+            title = "title",
+            subtitle = "subtitle",
+            ranks = 1,
+            date = "2023-1-1"
+        ),
+        onItemClick = {}
+    )
+}
+
 /**
  * Scrap item
  * click시 상세페이지로 이동
  */
 @Composable
-fun ArticleHeader(
+fun ScrapItem(
     modifier: Modifier = Modifier,
     news: ScrapResponse,
     onItemClick: (Int) -> Unit
 ) {
-    Row(
-        modifier
+
+    Column(
+        modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 20.dp, vertical = 11.dp)
+            .padding(horizontal = 23.dp, vertical = 13.dp)
             .clickable {
                 onItemClick(news.briefingId)
             },
-        verticalAlignment = Alignment.Top,
-        horizontalArrangement = Arrangement.SpaceBetween
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
     ) {
-        Column(
-            modifier = Modifier.widthIn(max = 161.dp),
-            verticalArrangement = Arrangement.Center,
-        ) {
-            Text(text = news.title, style = BriefingTheme.typography.SubtitleStyleBold)
-            Spacer(modifier = Modifier.height(5.dp))
-            Text(
-                text = news.subtitle,
-                style = BriefingTheme.typography.DetailStyleRegular,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
         Text(
-            text = "${news.date} #" + news.ranks, style = BriefingTheme.typography.DetailStyleRegular.copy(
-                color = BriefingTheme.color.PrimaryBlue,
-                lineHeight = 15.sp
+            text = "${news.title} | 이슈 #${news.title} | ${news.title}로 생성됨",
+            style = BriefingTheme.typography.DetailStyleRegular.copy(
+                color = BriefingTheme.color.TextGray
+            )
+        )
+        Spacer(modifier = Modifier.height(14.dp))
+        Text(
+            text = news.title,
+            style = BriefingTheme.typography.SubtitleStyleBold,
+        )
+        Text(
+            text = news.subtitle,
+            style = BriefingTheme.typography.ContextStyleRegular25.copy(
+                color = BriefingTheme.color.TextGray,
             )
         )
     }
